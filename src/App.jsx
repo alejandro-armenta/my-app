83445e7ba06dbb0a3b4e21218ef8171cb52e433c
Alejandro <armentaayalaalejandro@gmail.com>
2026-07-08 21:09:27 -0600

    made styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,28 @@
 import { useState } from "react"
 
-export default function App() {
+function ColoredText() {
 
-    const INITIAL_PRODUCTS = [
-        { id: 'p1', title: 'React - The Complete Guide [Course]', price: 19.99 },
-        { id: 'p2', title: 'Stylish Chair', price: 329.49 },
-        { id: 'p3', title: 'Ergonomic Chair', price: 269.99 },
-        { id: 'p4', title: 'History Video Game Collection', price: 99.99 },
-    ];
+    const [chosenPriority, setChosenPriority] = useState('low-prio')
 
-    const [products, setProducts] = useState(INITIAL_PRODUCTS)
-
-    function handleAddProduct() {
-        setProducts((prevProducts) => ([
-            ...prevProducts,
-            {
-                id: new Date().getTime(),
-                title: 'ale',
-                price: 500,
-            },
-        ]))
+    function handleChoosePriority(event) {
+        setChosenPriority(event.target.value)
     }
 
     return (
-        <section>
-            <button onClick={handleAddProduct}>Add Product</button>
-            <ul>
-                {products.map(
-                    product => (
-                        <li key={product.id}>
-                            {product.title} ${product.price}
-                        </li>
-                    )
-                )}
-            </ul>
-        </section>
+        <>
+            <p className={chosenPriority}>Chosen priority: {chosenPriority}</p>
+            <select onChange={handleChoosePriority}>
+                <option value="low-prio">Low</option>
+                <option value="high-prio">High</option>
+            </select>
+        </>
+    )
+}
+
+export default function App() {
+    return (
+        <>
+            <ColoredText />
+        </>
     )
 }
--- temp/App.jsx
+++ temp/App.jsx
@@ -2,6 +2,60 @@ import { useState } from "react";
 import Calculation from "./components/Calculation";
 import Result from "./components/Result";
 
+function ColoredText() {
+
+    const [enteredColor, setEnteredColor] = useState('')
+
+    function handleChangeColor(event) {
+        setEnteredColor(event.target.value)
+    }
+
+    return (
+        <>
+            <input type="text" onChange={handleChangeColor} />
+            <p style={{ color: enteredColor }}>hello</p>
+        </>
+    )
+}
+
+
+export default function App() {
+
+    const INITIAL_PRODUCTS = [
+        { id: 'p1', title: 'React - The Complete Guide [Course]', price: 19.99 },
+        { id: 'p2', title: 'Stylish Chair', price: 329.49 },
+        { id: 'p3', title: 'Ergonomic Chair', price: 269.99 },
+        { id: 'p4', title: 'History Video Game Collection', price: 99.99 },
+    ];
+
+    const [products, setProducts] = useState(INITIAL_PRODUCTS)
+
+    function handleAddProduct() {
+        setProducts((prevProducts) => ([
+            ...prevProducts,
+            {
+                id: new Date().getTime(),
+                title: 'ale',
+                price: 500,
+            },
+        ]))
+    }
+
+    return (
+        <section>
+            <button onClick={handleAddProduct}>Add Product</button>
+            <ul>
+                {products.map(
+                    product => (
+                        <li key={product.id}>
+                            {product.title} ${product.price}
+                        </li>
+                    )
+                )}
+            </ul>
+        </section>
+    )
+}
 
 function ProductsList({ products }) {
     return (
