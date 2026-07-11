6fc96579941fbe498445c82a87d7e3a9aebbb2f4
Alejandro <armentaayalaalejandro@gmail.com>
2026-07-11 15:27:00 -0600

    finished sidebar

--- src/components/SideDrawer.jsx
+++ src/components/SideDrawer.jsx
@@ -1,14 +1,31 @@
+import { createPortal } from 'react-dom'
 import classes from './SideDrawer.module.css'
 
 export default function SideDrawer({ onClose }) {
-    return (
+    return createPortal(
         <>
             <div className={classes.backdrop} onClick={onClose} />
 
             <aside className={classes.drawer}>
-            
-            </aside>
+                <nav>
+                    <ul>
 
-        </>
+                        <li>
+                            <a href='/'>Dashboard</a>
+                        </li>
+
+                        <li>
+                            <a href='/products'>Products</a>
+                        </li>
+
+                        <li>
+                            <a href='/profile'>Your Profile</a>
+                        </li>
+
+                    </ul>
+                </nav>
+            </aside>
+        </>,
+        document.getElementById('drawer')
     )
 }
